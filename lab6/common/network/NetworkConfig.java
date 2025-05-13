b1d67ef6fc395d623c6a298a62dd8eb34969e39b
MultiExima <fedorsv1999@gmail.com>
2025-05-13 18:43:07 +0300

    Update NetworkConfig.java

--- lab6/common/network/NetworkConfig.java
+++ lab6/common/network/NetworkConfig.java
@@ -6,6 +6,5 @@ package lab6.common.network;
 public class NetworkConfig {
     public static final int DEFAULT_PORT = 9999;
     public static final String DEFAULT_HOST = "localhost";
-    public static final int BUFFER_SIZE = 1024 * 1024; // 1MB
 } 
 
